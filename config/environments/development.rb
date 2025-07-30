ad2b102f962a4eca73b48bc8a110b02b286cb324
takahiro-okuda <takahiro.okuda@ventus-inc.com>
2025-07-30 16:56:37 +0900

    mailhogからmailpitへ変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost", port: 3001 }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: "mailhog",
+    address: "mail",
     port: 1025
   }
 
